d15a0a19e50f897ded876dc1ea2844824c2eda1d
Mariano Losada <89931647+marianodip12@users.noreply.github.com>
2026-03-23 00:43:32 -0300

    Update supabase.js

--- handball-pro-v8/src/lib/supabase.js
+++ handball-pro-v8/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
+const SUPABASE_URL = 'https://fpgojijkzkkhlfqfardn.supabase.co'
 
-const SUPABASE_URL = 'https://fpgojijzkkhlfqfardn.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_Mpf4slzcVyGWkQqNOeugFQ_a79k2iq5'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
